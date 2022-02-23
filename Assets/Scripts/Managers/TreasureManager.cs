4aaf20c160dcc28d8e3a9fecbf9bad2123583ace
parsa <48556656+parsarafiee@users.noreply.github.com>
2022-02-23 18:49:24 -0500

    treasure layer

--- Assets/Scripts/Managers/TreasureManager.cs
+++ Assets/Scripts/Managers/TreasureManager.cs
@@ -12,7 +12,8 @@ public class TreasureManager : Manager<TreasureManager, Treasure>
         for (int i = 0; i < GameLinks.Instance.respawnPositions.Length; i++)
         {
             GameObject obj = GameObject.Instantiate(treasurePrefab,GameLinks.Instance.respawnPositions[i].position  ,Quaternion.identity,GameLinks.Instance.TreasursParents);
-       
+
+            Debug.Log("sDSD");
             colletion.Add(obj.GetComponent<Treasure>());
 
         }
