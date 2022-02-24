b935216fc2425d92768c11ffdce31e5b06559293
DeskTopDarwin <francis.a.gaming@outlook.com>
2022-02-23 20:12:26 -0500

    Added script to manager
    created script for cube tiles + added code to LODManager

--- Assets/Scripts/Managers/LODManager.cs
+++ Assets/Scripts/Managers/LODManager.cs
@@ -3,35 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class LODManager : Manager
+public class LODManager : Manager<LODManager, CubeTile>
 {
-    #region singleton
+    public Vector3 mainBox;
+    public GameObject box;
 
-    private static LODManager instance;
-    public static LODManager Instance => instance ??= new LODManager();
-    private LODManager() { }
-    #endregion
+    public Vector3 divBoxVariables;
 
-    private LODRender lod;
-    
+    float x;
+    float y;
+    float z;
 
     public override void Init()
     {
-        lod = new LODRender();  
-        lod.Init();
+
+        
     }
 
     public override void PostInit()
     {
-        lod.PostInit();
+        //*to rework
+        x = mainBox.x / divBoxVariables.x;
+        y = mainBox.y / divBoxVariables.y;
+        z = mainBox.z / divBoxVariables.z;
+
+        Vector3 centerPos = new Vector3(-(mainBox.x / divBoxVariables.x) / 2, -(mainBox.y / divBoxVariables.y) / 2, -(mainBox.z / divBoxVariables.z) / 2);
+        for (int u = 0; u < divBoxVariables.x; u++)
+        {
+            centerPos.x += x;
+            for (int o = 0; o < divBoxVariables.y; o++)
+            {
+                centerPos.y += y;
+                for (int p = 0; p < divBoxVariables.z; p++)
+                {
+                    centerPos.z += z;
+                    box.transform.localScale = new Vector3(x, y, z);
+                    GameObject.Instantiate(box, new Vector3(centerPos.x, centerPos.y, centerPos.z), Quaternion.identity);
+
+                }
+
+                centerPos.z = -(mainBox.z / divBoxVariables.z) / 2;
+            }
+
+            centerPos.y = -(mainBox.y / divBoxVariables.y) / 2;
+        }
     }
 
-    public override void Refresh()
-    {
-        lod.Refresh();  
-    }
-    public override void FixedRefresh()
-    {
-        lod.FixedRefresh();
-    }
+
 }
